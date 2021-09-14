080205c410f9de01d28c5588cc52613ceb79b4d0
bajju <bajju@lendi.com.au>
2021-09-14 22:18:30 +1000

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [url, setUrl] = useState(null);
+  useEffect(() => {
+    /* eslint-disable no-undef */
+    chrome.tabs.onHighlighted.addListener((tab) => {
+      console.log("tab", tab);
+      setUrl(tab.url);
+    });
+    chrome.tabs.onActivated.addListener((activeInfo) => {
+      console.log("activeInfo", activeInfo);
+    });
+    // chrome.tabs..addListener((activeInfo) => {
+    //   console.log("activeInfo", activeInfo);
+    // });
+    /* eslint-disable no-undef */
+    chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+      console.log("tab", tab);
+      setUrl(tab.url);
+      // if (tab.url.match(/lendi/)) {
+      //   alert("browing lendi");
+      // }
+    });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App">{url ? <div>{url}</div> : <div>...loading</div>}</div>
   );
 }
 
